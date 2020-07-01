374bd631c8611ae04ee01dcb8944431cc94863c1
MariaSobol <sablemari@gmail.com>
2020-07-01 18:33:37 +0300

    Домашнее задание №6.

--- services/Session.php
+++ services/Session.php
@@ -6,29 +6,24 @@ namespace app\services;
 
 class Session
 {
-    protected function setSession(){
-        if (is_null($_SESSION)){
-            session_start();
-        }
+    public function __construct()
+    {
+        session_start();
     }
 
     public function setParam($key, $value){
-        $this->setSession();
         $_SESSION[$key] = $value;
     }
 
     public function getParam($key){
-        $this->setSession();
         return $_SESSION[$key];
     }
 
     public function deleteParam($key){
-        $this->setSession();
         unset($_SESSION[$key]);
     }
 
     public function deleteSession(){
-        $this->setSession();
         foreach ($_SESSION as $key => $value){
             unset($_SESSION[$key]);
         }
